20b8b62c6db94382478fbc64d2b1e235d59df062
Layla Jooya <laylajooya@gmail.com>
2019-06-18 14:15:39 -0500

    a lil marker added

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,27 +1,62 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import "./style.css";
+import { Z_BLOCK } from "zlib";
 
 const API_KEY = process.env.REACT_APP_GMAPS_API_KEY;
 
 const mapStyles = {
-  width: "75%",
-  height: "75%"
+  width: "100%",
+  height: "100%"
 };
 
 
 export class MapContainer extends Component {
+
+  state= {
+    activeMarker: {},
+    selectedPlace: {},
+    showingInfoWindow: false
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow){
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <Map
         google={this.props.google}
         zoom={10}
         style={mapStyles}
-        initialCenter={{
-          lat: 30.3076863,
-          lng: -97.8934866
-        }}
-      />  
+        initialCenter={{ lat: 30.3076863, lng: -97.8934866}}
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"this better work"}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map> 
     );
   }
 }
