25b1b3ba447c9cd2d4e66d989e9d0fbf7826855a
Layla Jooya <laylajooya@gmail.com>
2019-06-22 13:00:42 -0500

    backend database get and post

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -18,6 +18,66 @@ export class MapContainer extends Component {
     markers: [],
   };
 
+  getData = () => {
+    var request = require("request");
+
+    var options = {
+      method: "GET",
+      url: "http://localhost:3000/api/location/locations",
+      headers: {
+        "cache-control": "no-cache",
+        Connection: "keep-alive",
+        "accept-encoding": "gzip, deflate",
+        Host: "localhost:3000",
+        "Postman-Token":
+          "4b5beafa-b5bc-4793-a566-b92fd9c80b3f,746e29c4-8563-4b1a-85cf-1be2f664c198",
+        "Cache-Control": "no-cache",
+        Accept: "*/*",
+        "User-Agent": "PostmanRuntime/7.13.0",
+      },
+    };
+
+    request(options, function(error, response, body) {
+      if (error) throw new Error(error);
+
+      console.log(body);
+    });
+  };
+
+  postData = (latitude, longitude, enteredDescription) => {
+    var request = require("request");
+
+    var options = {
+      method: "POST",
+      url: "http://localhost:3000/api/location/locations",
+      headers: {
+        "cache-control": "no-cache",
+        Connection: "keep-alive",
+        "content-length": "45",
+        "accept-encoding": "gzip, deflate",
+        Host: "localhost:3000",
+        "Postman-Token":
+          "30b42cd3-c9fa-4608-84f1-9172d36f289a,44a0bfed-13a2-44f9-a048-ea33f8ea7284",
+        "Cache-Control": "no-cache",
+        Accept: "*/*",
+        "User-Agent": "PostmanRuntime/7.13.0",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        latitude: latitude,
+        longitude: longitude,
+        tag: enteredDescription,
+      },
+    };
+
+    request(options, function(error, response, body) {
+      if (error) throw new Error(error);
+      console.log("hey");
+
+      console.log(body);
+    });
+  };
+
   onMarkerClick = (props, marker, e) => {
     const enteredDescription = prompt(
       "Enter the description for this pin",
@@ -29,7 +89,7 @@ export class MapContainer extends Component {
       showingInfoWindow: true,
     });
   };
-  
+
   onClose = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
@@ -41,17 +101,18 @@ export class MapContainer extends Component {
 
   onMapClicked = (event, location, map) => {
     let latitude = event.latLng.lat();
-    let longtitude = event.latLng.lng();
+    let longitude = event.latLng.lng();
 
     this.setState({
       markers: [
         {
           lat: latitude,
-          lng: longtitude
+          lng: longitude,
         },
         ...this.state.markers,
       ],
     });
+    this.postData(latitude, longitude);
   };
 
   renderMarkers = () =>
@@ -64,12 +125,13 @@ export class MapContainer extends Component {
             lng: marker.lng,
           }}
           onClick={this.onMarkerClick}
-
         />
       );
     });
 
   render() {
+    this.getData();
+    this.postData();
     return (
       <GoogleMap
         onClick={this.onMapClicked}
---
+++ models/location.js
@@ -0,0 +1,24 @@
+// Burger models
+
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
+
+module.exports = function(sequelize, DataTypes) {
+  var Location = sequelize.define("Location", {
+    latitude: {
+      type: DataTypes.DECIMAL(7,5),
+      required: true
+    },
+    longitude: {
+      type: DataTypes.DECIMAL(8,5),
+      required: true
+    },
+    tag: {
+      type: DataTypes.STRING,
+      required: true
+    }
+
+  
+  });
+  return Location;
+};
