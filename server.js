5ece7508273dcbad39ee33c3bcbdbd8575163359
Layla Jooya <layla.jooya@gmail.com>
2019-08-16 08:21:15 -0500

    nevermind

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(routes);
 
 // Start the API server
 // ADD SEQUELIZE HERE TO CONNECT TO YOUR DB
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
